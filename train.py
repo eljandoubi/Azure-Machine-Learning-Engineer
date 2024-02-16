800b2e884a0c8bb0787a909855e764c872da8453
AbdelKarim ELJANDOUBI <78537694+eljandoubi@users.noreply.github.com>
2024-02-16 11:35:42 +0100

    add seed

--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 from sklearn.tree import RandomForestClassifier
 import argparse
 import os
-import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 from sklearn.metrics import roc_auc_score
+
+
+seed=42
 
 
 def get_data():
@@ -21,20 +23,21 @@ def get_data():
     x_df = pd.get_dummies(x_df, drop_first=True)
     y_df = x_df.pop("Attrition")
     
-    return train_test_split(x_df,y_df,test_size=0.2, random_state=42)
+    return train_test_split(x_df,y_df,test_size=0.2, random_state=seed)
 
 
 
 def main(args,run):
     """The trainer"""
 
-    run.log("Attribute selection measure:", np.str(args.criterion))
-    run.log("Split Strategy:", np.str(args.splitter))
-    run.log("Maximum Depth of a Tree:", np.float(args.max_depth))
+    run.log("Attribute selection measure:", args.criterion)
+    run.log("Bootstrap Strategy:", args.bootstrap)
+    run.log("Maximum Depth of a Tree:", args.max_depth)
 
     model = RandomForestClassifier(criterion=args.criterion,
                                    max_depth=args.max_depth,
-                                   bootstrap=args.bootstrap
+                                   bootstrap=args.bootstrap,
+                                   random_state=seed,
                                    )
     
     x_train,x_test,y_train,y_test=get_data()
@@ -42,7 +45,7 @@ def main(args,run):
     model.fit(x_train, y_train)
 
     AUC_weighted = roc_auc_score(y_test, model.predict_proba(x_test)[:, 1], average="weighted")
-    run.log("AUC_weighted", np.float(AUC_weighted))
+    run.log("AUC_weighted", AUC_weighted)
     
     os.makedirs('./outputs', exist_ok=True)    
     joblib.dump(value=model, filename='./outputs/model.joblib')
@@ -58,13 +61,15 @@ if __name__ == '__main__':
                         
     parser.add_argument('--bootstrap', type=bool, default=True, 
                         help="Whether bootstrap samples are used when building trees.",
-                        choices=["best","random"]
                         )
     
     parser.add_argument('--max_depth', type=int, default=None,
                         help="The maximum depth of the tree.")
 
     amgs = parser.parse_args()
+    
+    if amgs.max_depth is not None:
+        amgs.max_depth+=1
     
     
     main(amgs,Run.get_context())
